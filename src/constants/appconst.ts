618fa229be77ee86f731279e7cd14e4af53e2a55
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2023-03-28 10:47:29 +0500

    Create appconst.ts

---
+++ src/constants/appconst.ts
@@ -0,0 +1,21 @@
+const AppConsts = {
+    // userManagement: {
+    //   defaultAdminUserName: 'admin',
+    // },
+    localization: {
+      defaultLocalizationSourceName: 'EBOOK',
+    },
+    // authorization: {
+    //   encrptedAuthTokenName: 'enc_auth_token',
+    // },
+  
+    //  Live Server
+    // appBaseUrl: 'http://5.161.113.248:97',
+    // remoteServiceBaseUrl: 'http://5.161.113.248:97',
+    //  Testing Server
+    appBaseUrl: 'http://5.161.113.248:97',
+    remoteServiceBaseUrl: 'http://5.161.113.248:97',
+  };
+  
+  // };
+  export default AppConsts;
