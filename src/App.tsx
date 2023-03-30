294a7b52a18d5ed490850dd2397a6fd8a2f985ba
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2023-03-30 17:57:21 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/login';
+import { BrowserRouter} from 'react-router-dom';
+import CustomRoute from './component/CustomRoute';
 function App() {
-  const routes = [
-    {
-      path: "/home",
-      exact: true,
-      Component:<Home/>
-    },
-    {
-      path: "/",
-      exact: true,
-      Component:<Login/>
-    },
-  ];
   return (
-    <div>
-      <Routes>
-      {routes.map((route, index) => (
-              <Route
-                key={index}
-                path={route.path}
-                element={route.Component}
-                // children={<route.sidebar />}
-              />
-            ))}
-      </Routes>
-    </div>
-  );
-}
-function Wrapped() {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+      <CustomRoute/>
+      </BrowserRouter>
   );
 }
 
-export default Wrapped;
+export default App
