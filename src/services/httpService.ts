495b334d11b79fafdfbc7b8811cf7e62430c5ff1
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2023-03-30 17:38:41 +0500

    Update httpService.ts

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,8 +1,7 @@
 import AppConsts from '.././constants/appconst';
 let token = localStorage.getItem('token');
-debugger
 import axios from 'axios';
-
+import { Modal } from 'antd';
 const http = axios.create({
   baseURL: AppConsts.remoteServiceBaseUrl,
   timeout: 800000,
@@ -11,4 +10,32 @@ const http = axios.create({
     'Content-Type': 'application/json',
   }
 });
+http.interceptors.request.use(
+  function (config) {
+    if (!token) {
+      debugger
+      config.headers.common['Authorization'] = 'Bearer ' + token;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+http.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.data.Message) {
+      Modal.error({
+        title: error.response.data.Message,
+        content: error.response.data.Message,
+        onOk() {
+          Modal.destroyAll();
+        },
+      });
+    }
+  }
+)
 export default http;
